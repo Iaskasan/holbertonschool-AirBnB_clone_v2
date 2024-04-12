0b34b23c7063732d076925631d00002a84a1bf12
Iaskasan <nokiesan@yahoo.fr>
2024-04-12 09:55:19 +0200

    added first tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
